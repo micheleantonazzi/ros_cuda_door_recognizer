1196488d646648e599a58cd3c41b9bc8553fdf16
micheleantonazzi <micheleantonazzi@gmail.com>
2019-07-07 15:58:56 +0200

    obtain image from opencv

--- src/utilities/parameters.cpp
+++ src/utilities/parameters.cpp
@@ -2,4 +2,28 @@
 // Created by michele on 07/07/19.
 //
 
+#include <ros/ros.h>
 #include "parameters.h"
+
+using namespace ros;
+
+string Parameters::DOOR_HIGH_RES = "/home/michele/catkin_ws/src/ros_cuda_door_recognizer/images/test/door_high_res.jpg";
+string Parameters::DOOR_MED_RES = "/home/michele/catkin_ws/src/ros_cuda_door_recognizer/images/test/door_med_res.jpg";
+
+Parameters::Parameters() : camera(false), topic(), image_path(){}
+
+void Parameters::getValues() {
+    NodeHandle nodeHandle("~");
+
+    nodeHandle.param<bool>("camera", this->camera, false);
+    nodeHandle.param<string>("image", this->image_path, DOOR_MED_RES);
+}
+
+bool Parameters::usingCamera() {
+    return this->camera;
+}
+
+Mat Parameters::getImageFromOpenCV() {
+    return this->camera ? Mat() :
+           imread(this->image_path);
+}
--- src/utilities/parameters.h
+++ src/utilities/parameters.h
@@ -2,12 +2,40 @@
 // Created by michele on 07/07/19.
 //
 
+#include <string>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
 #ifndef ROS_CUDA_DOOR_RECOGNIZER_PARAMETERS_H
 #define ROS_CUDA_DOOR_RECOGNIZER_PARAMETERS_H
 
 
-class parameters {
+class Parameters {
+private:
 
+    static string DOOR_HIGH_RES;
+    static string DOOR_MED_RES;
+
+    // If you want to analise a frame acquired by camera set the variable true, otherwise false
+    bool camera;
+
+    // If you are using the camera specify the image's topic
+    string topic;
+
+    // If you don't use the camera specify the image's path
+    string image_path;
+
+public:
+    Parameters();
+
+    // Acquires parameter's values
+    void getValues();
+
+    bool usingCamera();
+
+    Mat getImageFromOpenCV();
 };
 
 
