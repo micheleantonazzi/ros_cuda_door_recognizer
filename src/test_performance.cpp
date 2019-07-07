85387355b160c23c864ee3fc081efb12b24bdf4e
micheleantonazzi <micheleantonazzi@gmail.com>
2019-07-07 11:22:36 +0200

    add new node to test performance

---
+++ src/test_performance.cpp
@@ -0,0 +1,12 @@
+//
+// Created by michele on 07/07/19.
+//
+
+#include <ros/ros.h>
+using namespace ros;
+
+int main(int argc, char **argv){
+
+    ros::init(argc, argv, "services");
+    ROS_INFO_STREAM("Started node to test performance");
+}
