3a91a2b4a45d44ae5abb5e1ac742ca8b6eb17626
micheleantonazzi <micheleantonazzi@gmail.com>
2019-07-07 12:45:12 +0200

    add test images

---
+++ src/utilities/parameters.cpp
@@ -0,0 +1,5 @@
+//
+// Created by michele on 07/07/19.
+//
+
+#include "parameters.h"
---
+++ src/utilities/parameters.h
@@ -0,0 +1,14 @@
+//
+// Created by michele on 07/07/19.
+//
+
+#ifndef ROS_CUDA_DOOR_RECOGNIZER_PARAMETERS_H
+#define ROS_CUDA_DOOR_RECOGNIZER_PARAMETERS_H
+
+
+class parameters {
+
+};
+
+
+#endif //ROS_CUDA_DOOR_RECOGNIZER_PARAMETERS_H
