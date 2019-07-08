a6f59785e2bfe0b9f7b55095624d27243aa70401
micheleantonazzi <micheleantonazzi@gmail.com>
2019-07-08 12:06:47 +0200

    test performance gray scale cpu

--- src/test_performance.cpp
+++ src/test_performance.cpp
@@ -3,10 +3,47 @@
 //
 
 #include <ros/ros.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "utilities/parameters.h"
+#include "utilities/image.h"
+#include "cpu/cpu_algorithms.h"
+#include "utilities/time_utilities.h"
+
 using namespace ros;
+using namespace cv;
 
 int main(int argc, char **argv){
 
     ros::init(argc, argv, "services");
-    ROS_INFO_STREAM("Started node to test performance");
+    ROS_INFO("Started node to test performance");
+
+    Parameters::getInstance().getValues();
+
+    Image *image = new Image();
+    image->acquireImage();
+
+    if(Parameters::getInstance().usingCamera()){
+
+    }
+    else {
+
+        //CPU
+
+        cout << "Analyze image from OpenCV:\n"
+                " - width: " << image->getWidth() << "\n" <<
+                " - height: " << image->getHeight() << "\n" <<
+                "Operations in CPU:\n";
+
+        double time_start_gray_scale = seconds();
+        CpuAlgorithms::getInstance().toGrayScale(image->getOpenCVImage().data, image->getWidth(), image->getHeight());
+        double time_end_gray_scale = seconds();
+        cout << " - convert to gray scale: " << time_end_gray_scale - time_start_gray_scale << "\n";
+
+        //GPU
+
+
+    }
+
+    delete image;
 }
