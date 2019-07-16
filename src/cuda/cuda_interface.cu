f55b75651969cc485c042737f8baea04187b2468
micheleantonazzi <micheleantonazzi@gmail.com>
2019-07-16 09:57:42 +0200

    create kernel that uses Pixel24

--- src/cuda/cuda_interface.cu
+++ src/cuda/cuda_interface.cu
@@ -15,6 +15,34 @@ void CudaInterface::test_cuda(){
     CHECK(cudaDeviceSynchronize());
 }
 
+Pixel24* CudaInterface::getPixelArray(unsigned char *imageData, int width, int height) {
+    int imageSize = width * height;
+
+    Pixel24 *pixelArray;
+
+    cudaMallocHost(&pixelArray, imageSize * sizeof(Pixel24));
+
+    for(int i = 0; i < imageSize; ++i){
+        pixelArray[i].R = *(imageData++);
+        pixelArray[i].G = *(imageData++);
+        pixelArray[i].B = *(imageData++);
+    }
+
+    return pixelArray;
+
+}
+
+void CudaInterface::pixelArrayToCharArray(unsigned char *imageData, Pixel24 *source, int width, int height) {
+    int imageSize = width * height;
+
+    for (int i = 0; i < imageSize; ++i) {
+        Pixel24 pixel = *(source++);
+        *(imageData++) = pixel.R;
+        *(imageData++) = pixel.G;
+        *(imageData++) = pixel.B;
+    }
+}
+
 __global__ void to_gray_scale(unsigned char *destination, unsigned char *source, int width, int height){
     int threadTot = gridDim.x * blockDim.x;
 
@@ -44,6 +72,46 @@ __global__ void to_gray_scale(unsigned char *destination, unsigned char *source,
 
 double CudaInterface::toGrayScale(unsigned char *destination, unsigned char *source, int width, int height, int numBlocks, int numThread) {
 
+    double time = seconds();
+
+    to_gray_scale<<<numBlocks, numThread>>>(destination, source, width, height);
+
+    cudaDeviceSynchronize();
+
+    time = seconds() - time;
+
+    return time;
+}
+
+__global__ void to_gray_scale(Pixel24 *destination, Pixel24 *source, int width, int height){
+    int threadTot = gridDim.x * blockDim.x;
+
+    int imageSize = width * height;
+
+    int valuesPerThread = (imageSize / threadTot) + 1;
+
+    int threadId = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if (threadId * valuesPerThread < imageSize){
+
+        // Move the pointer to the correct position
+        source += threadId * valuesPerThread;
+        destination += threadId * valuesPerThread;
+
+        int start = threadId * valuesPerThread;
+
+        for(int i = 0; i < valuesPerThread && start + i < imageSize; i++){
+            Pixel24 pixel24 = *(source++);
+            unsigned char average = (pixel24.R + pixel24.G + pixel24.B) / 3;
+            pixel24.R = average;
+            pixel24.G = average;
+            pixel24.B = average;
+            *(destination++) = pixel24;
+        }
+    }
+}
+
+double CudaInterface::toGrayScale(Pixel24 *destination, Pixel24 *source, int width, int height, int numBlocks, int numThread) {
     double time = seconds();
 
     to_gray_scale<<<numBlocks, numThread>>>(destination, source, width, height);
--- src/cuda/cuda_interface.h
+++ src/cuda/cuda_interface.h
@@ -2,6 +2,8 @@
 // Created by michele on 28/05/19.
 //
 
+#include "../utilities/pixel24.h"
+
 #ifndef ROS_CUDA_DOOR_RECOGNIZER_CUDA_INTERFACE_H
 #define ROS_CUDA_DOOR_RECOGNIZER_CUDA_INTERFACE_H
 
@@ -11,9 +13,18 @@ public:
 
     static void test_cuda();
 
+    // Build an array of Pixel24 that contains image data.
+    // The array is allocated in PINNED MEMORY
+    static Pixel24* getPixelArray(unsigned char *imageData, int width, int height);
+
+    static void pixelArrayToCharArray(unsigned char *imageData, Pixel24 *source, int width, int height);
+
     // Convert an image in gray scale
     // Return the time to execute del kernel
     static double toGrayScale(unsigned char *destination, unsigned char *source, int width, int height, int numBlocks, int numThread);
+
+    // Convert an image in gray scale using type Pixel32 to improve the access memory performance
+    static double toGrayScale(Pixel24 *destination, Pixel24 *source, int width, int height, int numBlocks, int numThread);
 };
 
 
