2936053867ed7d58b65685e022ed2461947550ca
micheleantonazzi <micheleantonazzi@gmail.com>
2019-07-16 09:25:22 +0200

    add type pixel24

---
+++ src/utilities/pixel24.h
@@ -0,0 +1,14 @@
+//
+// Created by michele on 16/07/19.
+//
+
+#ifndef ROS_CUDA_DOOR_RECOGNIZER_PIXEL24_H
+#define ROS_CUDA_DOOR_RECOGNIZER_PIXEL24_H
+
+typedef struct{
+    unsigned char R;
+    unsigned char G;
+    unsigned char B;
+} Pixel24;
+
+#endif //ROS_CUDA_DOOR_RECOGNIZER_PIXEL24_H
