e7e6356b14afe4bed702e6d34915882853ce4eee
micheleantonazzi <micheleantonazzi@gmail.com>
2019-07-18 10:44:53 +0200

    remove cout

--- src/door_recognizer.cpp
+++ src/door_recognizer.cpp
@@ -21,7 +21,6 @@ int main(int argc, char **argv){
     ROS_INFO_STREAM("Started door recognizer node");
 
     Parameters::getInstance().getValues();
-    cout << Parameters::getInstance().getTopic() << endl;
 
     NodeHandle node;
 
